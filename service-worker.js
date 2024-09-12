21c244dac93bbe64a344477e6b05529bb4dbccb9
SreehariAjesh <157208780+SreehariAjesh@users.noreply.github.com>
2024-09-12 12:00:00 +0530

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -1,8 +1,8 @@
 const cacheName = 'pwa-cache-v1';
 const filesToCache = [
   '/',
-  '/index.html',
-  '/styles.css',
+  'index.html',
+  'styles.css',
   'images/6262d795-9c1a-4c99-9423-cd1499fce84c.png',
   'images/6262d795-9c1a-4c99-9423-cd1499fce84c (1).png'
 ];
@@ -19,10 +19,10 @@ self.addEventListener('install', event => {
   event.waitUntil(
     caches.open('panni-cache').then(cache => {
       return cache.addAll([
-        '/index.html',
-        '/styles.css',
-        '/app.js',
-        '/manifest.json',
+        'index.html',
+        'styles.css',
+        'app.js',
+        'manifest.json',
         'images/6262d795-9c1a-4c99-9423-cd1499fce84c.png',
         'images/6262d795-9c1a-4c99-9423-cd1499fce84c (1).png'
       ]);
