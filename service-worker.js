e1fa8ac3c8dece3d48b1476040144dd8a22af701
SreehariAjesh <157208780+SreehariAjesh@users.noreply.github.com>
2024-09-12 11:54:39 +0530

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -50,13 +50,4 @@ window.addEventListener('beforeinstallprompt', (e) => {
     });
   });
 });
-if ('serviceWorker' in navigator) {
-  window.addEventListener('load', function() {
-    navigator.serviceWorker.register('/service-worker.js').then(function(registration) {
-      console.log('ServiceWorker registration successful with scope: ', registration.scope);
-    }, function(err) {
-      console.log('ServiceWorker registration failed: ', err);
-    });
-  });
-}
 
